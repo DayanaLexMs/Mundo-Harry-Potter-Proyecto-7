807b09948998ac85282f1ce934cda0d765a0909e
LuciaCuellar <luciadelmarcg@gmail.com>
2026-06-01 17:16:41 -0500

    Hice la conexión esta pa lo del juego

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,18 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package principal;
+import controlador.JuegoControlador;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-/**
- *
- * @author USUARIO
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+            }
+            new JuegoControlador().iniciar();
+        });
     }
-    
 }
